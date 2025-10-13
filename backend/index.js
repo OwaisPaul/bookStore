8edb2bec88f9af3079aadbd71e8c30302476e0a2
OwasiPaul <ovaish25@gmail.com>
2025-10-13 08:51:36 +0530

    get one book by id

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,19 @@ app.post('/books', async (request, response) => {
     }
  })
 
+ // getting one book from database by id
+  app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+ })
+
 
 mongoose.connect(mongodbURL)
         .then(() => {
