4d34832b32477aed7da0dd984c0af88e78cd5232
OwasiPaul <ovaish25@gmail.com>
2025-10-18 09:34:56 +0530

    CORS policy

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import express from "express"
 import { PORT, mongodbURL } from "./config.js"
 import mongoose from 'mongoose'
-import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors'
 
 const app = express()
 
 //middleware for parsing request body
 app.use(express.json());
+
+//middleware for handling CORS policy
+//Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+//Option 2: Allow custom origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
 
 app.get('/', (request,response) => {
     console.log(request)
